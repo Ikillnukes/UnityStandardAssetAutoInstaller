ded4598a3a58333f3856dbc36410179d418d7d6b
Álvaro Rodríguez García <ikillnukes1@gmail.com>
2016-05-08 19:42:05 +0200

    Initial commit

---
+++ Assets/S. Asset Auto-Installer/StandardInstaller/Helpers.cs
@@ -0,0 +1,21 @@
+﻿public static class Helpers
+{
+    #region "String Extensions"
+    public static string FirstCharToUpper(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return "";
+        return input[0].ToString().ToUpper() + input.Substring(1);
+    }
+    public static string ReplaceLast(this string Source, string Find, string Replace)
+    {
+        int place = Source.LastIndexOf(Find);
+
+        if (place == -1)
+            return Source;
+
+        string result = Source.Remove(place, Find.Length).Insert(place, Replace);
+        return result;
+    }
+    #endregion
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# UnityStandardAssetAutoInstaller
+
+##STANDARD ASSET AUTO-INSTALLER for Unity
+
+###Introduction
+
+With this asset you can prepare Standard Assets to be imported on your next assets.
+The purpouse of it, is avoid include unnecesary files when you create a .unitypackage or your submit your assets to the Unity Asset Store.
